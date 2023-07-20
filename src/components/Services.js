8fc29c768acb0766e8c70234d3afcbf6a43634ed
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2023-07-21 00:37:23 +0800

    Update services page.

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,8 +11,96 @@ const Services = () => {
       data-nav-tooltip="Services"
       className="pp-section pp-scrollable section"
     >
-      <div className="container">
+    <div className="container">
         <div className="title">
+          <h3>Experience.</h3>
+        </div>
+        <div className="resume-box">
+          <div className="resume-row">
+            <div className="row">
+              <div className="col-sm-3 col-md-3 col-xl-2">
+                <div className="rb-left">
+                  <img src="static/img/tritech-logo.png" title="" alt="" />
+                </div>
+              </div>
+              <div className="col-sm-9 col-md-9 col-xl-10">
+                <div className="rb-right">
+                  <h6>Mobile Application Developer</h6>
+                  <label>Tritech Retail | Remote | May 2021 - Present</label>
+                  <div className="rb-time">Full Time</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="resume-row">
+            <div className="row">
+              <div className="col-sm-3 col-md-3 col-xl-2">
+                <div className="rb-left">
+                  <img src="static/img/element7-logo.jpeg" title="" alt="" />
+                </div>
+              </div>
+              <div className="col-sm-9 col-md-9 col-xl-10">
+                <div className="rb-right">
+                  <h6>Full Stack Software Engineer</h6>
+                  <label>Element7 | Remote | June 2022 - November 2022</label>
+                  <div className="rb-time">Freelance</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="resume-row">
+            <div className="row">
+              <div className="col-sm-3 col-md-3 col-xl-2">
+                <div className="rb-left">
+                  <img src="static/img/ncr-logo.jpeg" title="" alt="" />
+                </div>
+              </div>
+              <div className="col-sm-9 col-md-9 col-xl-10">
+                <div className="rb-right">
+                  <h6>Software Engineer - Mobile Application Developer</h6>
+                  <label>NCR Corporation | On Site | May 2019 - May 2021</label>
+                  <div className="rb-time">Full Time</div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="resume-row">
+            <div className="row">
+              <div className="col-sm-3 col-md-3 col-xl-2">
+                <div className="rb-left">
+                  <img src="static/img/accenture-logo.jpeg" title="" alt="" />
+                </div>
+              </div>
+              <div className="col-sm-9 col-md-9 col-xl-10">
+                <div className="rb-right">
+                  <h6>Associate Software Engineer</h6>
+                  <label>Accenture Inc., | On Site | October 2017 - May 2019</label>
+                  <div className="rb-time">Full Time</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="resume-row">
+            <div className="row">
+              <div className="col-sm-3 col-md-3 col-xl-2">
+                <div className="rb-left">
+                  <img src="static/img/yoshii-logo.jpeg" title="" alt="" />
+                </div>
+              </div>
+              <div className="col-sm-9 col-md-9 col-xl-10">
+                <div className="rb-right">
+                  <h6>Software Developer Intern</h6>
+                  <label>Yoshii Software Solutions | On Site | April 2016 - October 2016</label>
+                  <div className="rb-time">Internship</div>
+                  <p>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="title">
           <h3>What I do?</h3>
         </div>
         <div className="row">
@@ -88,9 +176,10 @@ const Services = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="separated" />
         <Testimonials />
+        <div className="separated" />
       </div>
     </section>
   );
