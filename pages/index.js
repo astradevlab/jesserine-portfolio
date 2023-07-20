b2719bca9c2724da1940e8509eb3b1dc2206fbe1
Chiong, Nicolas <njcolaschiong@gmail.com>
2023-07-20 15:36:50 +0800

    change styling

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,68 @@
-import Header from "@/src/components/Header";
-import ScrollBar from "@/src/components/ScrollBar";
-import About from "@/src/components/sections/About";
-import Blog from "@/src/components/sections/Blog";
-import Clients from "@/src/components/sections/Clients";
-import Contact from "@/src/components/sections/Contact";
-import Copyright from "@/src/components/sections/Copyright";
-import Facts from "@/src/components/sections/Facts";
-import Home from "@/src/components/sections/Home";
-import Portfolio from "@/src/components/sections/Portfolio";
-import Testimonials from "@/src/components/sections/Testimonials";
-import Separator from "@/src/components/Separator";
-import { jqueryFuntion } from "@/src/utilits";
-import { Fragment, useEffect } from "react";
+import dynamic from "next/dynamic";
+import About from "../src/components/About";
+import Blog from "../src/components/Blog";
+import Contact from "../src/components/Contact";
+import Services from "../src/components/Services";
+import TypingAnimation from "../src/components/TypingAnimation";
+import Layout from "../src/layout/Layout";
+const Portfolio = dynamic(() => import("../src/components/Portfolio"), {
+  ssr: false,
+});
 const Index = () => {
-  useEffect(() => {
-    jqueryFuntion();
-  });
-
   return (
-    <Fragment>
-      <div className="page-content">
-        <Header />
-        <div id="wrapper">
-          <main className="flex-column-mobile">
-            <Home />
-            <About />
-            <Separator type={"down"} />
-            <Facts />
-            <Separator type={"up"} />
-            <Portfolio />
-            <Separator type={"down"} />
-            <Testimonials />
-            <Separator type={"up"} />
-            <Contact />
-            <Separator type={"down"} />
-            <Clients />
-            <Separator type={"up"} />
-            <Blog />
-            <Separator type={"down"} />
-            <Copyright />
-          </main>
+    <Layout>
+      <section
+        id="home"
+        data-nav-tooltip="Home"
+        className="pp-section pp-scrollable"
+      >
+        <div className="home-banner">
+          <div className="container">
+            <div className="row full-screen align-items-center">
+              <div className="col-lg-6">
+                <div className="type-box">
+                  <h6>Hello, I am</h6>
+                  <h1 className="font-alt">Sergio Gadot</h1>
+                  <p className="lead">
+                    I Am Passionate <TypingAnimation />
+                  </p>
+                  <p className="desc">
+                    I design and develop services for customers of all sizes,
+                    specializing in creating stylish, modern websites, web
+                    services and online stores.
+                  </p>
+                  <div className="btn-bar">
+                    <a className="px-btn px-btn-theme" href="#">
+                      Donwload CV
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="hb-img">
+                  <img src="static/img/home-banner.png" title="" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <ScrollBar />
-      </div>
-    </Fragment>
+      </section>
+      {/* End Home */}
+      {/* about us */}
+      <About />
+      {/* End about us */}
+      {/* Services */}
+      <Services />
+      {/* End Services */}
+      {/* Portfolio */}
+      <Portfolio />
+      {/* End Portfolio */}
+      {/* Blog */}
+      <Blog />
+      {/* End Blog */}
+      {/* Contact us */}
+      <Contact />
+    </Layout>
   );
 };
 export default Index;
