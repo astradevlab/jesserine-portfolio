31192d06f362065dea173ab9e52aae049e8b9e1b
Jesserine Lopez <dev.jesserinelopez@gmail.com>
2023-07-21 00:37:39 +0800

    Update testimonials page.

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -34,58 +34,61 @@ const Testimonials = () => {
       <Swiper {...props}>
         <SwiperSlide className="testimonial-01 media">
           <div className="avatar">
-            <img src="static/img/team-1.jpg" title="" alt="" />
+            <img src="static/img/luis.jpeg" title="" alt="" />
           </div>
           <div className="media-body">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the {`industry's`} standard dummy
-              text ever since the 1500s.
+            Jesserine is a kind, tolerant, considerate, understanding, honest, resilient and reliable person, 
+            that can bring great value to any team in all of technical skills, soft skills, positive culture and human traits.
+            She is both a good software engineer and project manager, with sensitivity and understanding to explore and specify 
+            requirements to address customer's needs while also addressing developer and team concerns concerns. Jesserine's 
+            human qualities together with her tech knowledge on cross-platform mobile development, web, server and APIs, 
+            synergize with her project management skills and make her a valuable asset in any software development team.
             </p>
-            <h6>Jennifer Lutheran</h6>
-            <span>CEO at pxdraft</span>
+            <h6>Luis Miguel Serrano</h6>
+            <span>CTO at Rise.IO</span>
           </div>
         </SwiperSlide>
         <SwiperSlide className="testimonial-01 media">
           <div className="avatar">
-            <img src="static/img/team-2.jpg" title="" alt="" />
+            <img src="static/img/romelyn.jpeg" title="" alt="" />
           </div>
           <div className="media-body">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the {`industry's`} standard dummy
-              text ever since the 1500s.
+            Jesserine is determined, intelligent, and is always up for big challenges. She always does her best and shows 
+            she is capable of bigger roles. As a new product owner, a heavy role indeed, she has done a great job of relaying 
+            and polishing requirements. Doing the PO role and at the same time a developer supporting customer issues, she 
+            manages to deliver both without sacrificing the quality. Definitely a great asset to the company.
             </p>
-            <h6>Jennifer Lutheran</h6>
-            <span>CEO at pxdraft</span>
+            <h6>Romelyn Nellas</h6>
+            <span>Software Engineer at Full Scale</span>
           </div>
         </SwiperSlide>
         <SwiperSlide className="testimonial-01 media">
           <div className="avatar">
-            <img src="static/img/team-3.jpg" title="" alt="" />
+            <img src="static/img/nick.jpeg" title="" alt="" />
           </div>
           <div className="media-body">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the {`industry's`} standard dummy
-              text ever since the 1500s.
+            What I like about Jess is that she is determined, dedicated, and focused on the things she does. 
+            She is an exemplary mobile software developer.
             </p>
-            <h6>Jennifer Lutheran</h6>
-            <span>CEO at pxdraft</span>
+            <h6>Nicolas James Chiong</h6>
+            <span>Technical Consultant at Compass Group USA</span>
           </div>
         </SwiperSlide>
         <SwiperSlide className="testimonial-01 media">
           <div className="avatar">
-            <img src="static/img/team-1.jpg" title="" alt="" />
+            <img src="static/img/jayson.jpeg" title="" alt="" />
           </div>
           <div className="media-body">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the {`industry's`} standard dummy
-              text ever since the 1500s.
+            Jesserine is a highly capable, highly motivated member of our development team! She is well organized, 
+            diligent, and a fast learner. She doesn't hesitate to asks great questions, and then provide solutions 
+            as well. She also handles unexpected roadblocks with ease and confidence. Great to have her on the team. 
             </p>
-            <h6>Jennifer Lutheran</h6>
-            <span>CEO at pxdraft</span>
+            <h6>Jayson Catubig</h6>
+            <span>Test Engineering Associate Manager at Accenture</span>
           </div>
         </SwiperSlide>
       </Swiper>
